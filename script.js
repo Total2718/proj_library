372594b8477d5c50b2e51028b13ac08b431fe203
Total <jlacenere@outlook.com>
2021-10-14 11:12:40 -0400

    The check to see if the submitted book is already present in the library has been added. The object constructor has also been started.

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-// let myLibrary = [];
+let myLibrary = [];
 
-// function Book(title) {
-//   let submission_form = document.getElementById("submit_button");
-//   let submitted_title = submission_form[0];
-//   let submitted_author = submission_form[1];
-//   let submitted_pages = submission_form[2];
-//   let submitted_read = submission_form[3];
-//   let new_book = {title: };
+function Book(title, author, pages, read) {
   
-// }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  addBookToLibrary(this);
+  // let submission_form = document.getElementById("submit_button");
+  // let submitted_title = submission_form[0];
+  // let submitted_author = submission_form[1];
+  // let submitted_pages = submission_form[2];
+  // let submitted_read = submission_form[3];
+  // let new_book = {title: submitted_title };
+  
+}
 
-// function addBookToLibrary(title) {
-//     myLibrary.push(title);
+function addBookToLibrary(book) {
+    myLibrary.push(book);
     
   
-// }
+}
 
 
 
@@ -25,12 +31,14 @@ let reg_title = "Title";
 let reg_author = "Author";
 let reg_pages = "Pages";
 
-let bold
+
 let card_container = document.getElementById("card_container");
 let lib_card = document.getElementsByClassName("library_card");
 
 let form_back_cover = document.getElementById('cover');
 let add_book_form = document.getElementById('pop_up_form');
+
+
 for (let i = 0; i < lib_card.length; i++) {
     let book_title = document.createElement("div");
     let author = document.createElement("div");
@@ -43,14 +51,7 @@ for (let i = 0; i < lib_card.length; i++) {
     lib_card[i].appendChild(author);
     lib_card[i].appendChild(pages);
   } 
-  let submit_button_a = document.getElementById("submit_button")
-  submit_button_a.addEventListener("click", trial);
-function trial(){
-  let submitted_title = document.getElementById('title_input_box').value;
-  let submitted_author = document.getElementById('author_input').value;
-  let submitted_pages = document.getElementById('pages_input').value;
-  let submitted_read = document.getElementById('read_input').checked;
-}
+ 
 
 //this is an event listener to make the pop up form appear
 let add_book_button = document.getElementById('add_book');
@@ -67,5 +68,33 @@ form_back_cover.addEventListener('click', form_disappear);
 function form_disappear(){
   form_back_cover.classList.remove('active');
   add_book_form.classList.remove('active');
+  add_book_form.reset();
 
 }
+
+
+let submit_button_a = document.getElementById("submit_button")
+submit_button_a.addEventListener("click", check_book);
+function check_book(){
+  let submitted_title = document.getElementById('title_input_box').value;
+  let submitted_author = document.getElementById('author_input').value;
+  let submitted_pages = document.getElementById('pages_input').value;
+  let submitted_read = document.getElementById('read_input').checked;
+  let title_flag = false;
+  
+  for (let i = 0; i < myLibrary.length; i++){
+    if (myLibrary[i].title == submitted_title){
+      title_flag = true;
+      alert("You already have this book in your library!");
+    }
+  }
+  if (title_flag == false){
+    const new_book = new Book(submitted_title, submitted_author, submitted_pages, submitted_read);
+  }
+console.log(myLibrary);
+form_disappear();
+}
+
+
+
+
